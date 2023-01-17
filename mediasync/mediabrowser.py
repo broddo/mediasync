487af55a189b5896bbcb5836761ffae3219eca78
broddo <barry.broderick@icloud.com>
2023-01-16 22:11:00 -0500

    Moved and renamed

--- mediasync/mediabrowser.py
+++ mediasync/mediabrowser.py
@@ -1,28 +1,30 @@
-import sessions
-import requests
+from mediasync import sessions
 
 
-class APIError(Exception):
+class MediaBrowserAPIError(Exception):
     def __init__(self, name, response, message):
-        code = f"{name} responded with {response.status_code}. {response.content.decode('utf-8')}"
+        code = f"{name} responded with {response.reason}. {response.content.decode('utf-8')}"
         super().__init__(f"{message}. {code}")
 
 
 class MediaBrowser:
-    def __init__(self, url, headers):
+    def __init__(self, name, url, headers):
+        self.name = name
         self.s = sessions.BaseUrlSession(url, headers)
+
+    def check_response(self, response, message):
+        if response.status_code != 200:
+            raise MediaBrowserAPIError(self.name, response, message)
 
     def get_users(self):
         response = self.s.get("/Users")
-        if response.status_code != 200:
-            raise APIError(self.name, response, "Failed to retrieve user list")
+        self.check_response(response, "Failed to retrieve user list")
         items = response.json()
         return {item["Name"]: item["Id"] for item in items}
 
     def get_item(self, id):
         response = self.s.get(f"/Items/{id}/ExternalIdInfos")
-        if response.status_code != 200:
-            raise APIError(self.name, response, f"Failed to retrieve item {id}")
+        self.check_response(response, f"Failed to retrieve item {id}")
         return response.json()
 
     def get_user_items(self, id, type, played=False):
@@ -33,10 +35,7 @@ class MediaBrowser:
             "Fields": "ProviderIds, Path",
         }
         response = self.s.get(f"/Users/{id}/Items", params=params)
-        if response.status_code != 200:
-            raise APIError(
-                self.name, response, f"Failed to retrieve items for user id '{id}'"
-            )
+        self.check_response(response, f"Failed to retrieve items for user id '{id}'")
         return response.json()["Items"]
 
     def get_items(self, type):
@@ -46,43 +45,35 @@ class MediaBrowser:
             "Fields": "ProviderIds, Path",
         }
         response = self.s.get(f"/Items", params=params)
-        if response.status_code != 200:
-            raise APIError(self.name, response, f"Failed to retrieve items")
+        self.check_response(response, f"Failed to retrieve items")
         return response.json()["Items"]
 
     def create_user(self, user, password):
         data = {"Name": user, "Password": password}
         response = self.s.post("/Users/New", json=data)
-        if response.status_code != 200:
-            raise APIError(self.name, response, f"Failed to create user '{user}'")
+        self.check_response(response, f"Failed to create user '{user}'")
         item = response.json()
         return {item["Name"]: item["Id"]}
 
     def set_watched(self, user_id, items):
         for item in items:
             response = self.s.post(f"/Users/{user_id}/PlayedItems/{item['Id']}")
-            if response.status_code != 200:
-                raise APIError(
-                    self.name,
-                    response,
-                    f"Failed to update watch status for item {item['Id']}, user '{user_id}'",
-                )
+            self.check_response(
+                response,
+                f"Failed to update watch status for item {item['Id']}, user '{user_id}'",
+            )
 
 
 class Emby(MediaBrowser):
-    name = "Emby"
-
     def __init__(self, url, key):
         headers = {"X-Emby-Token": key}
-        super().__init__(url, headers)
+        super().__init__("Emby", url, headers)
 
 
 class Jellyfin(MediaBrowser):
-    name = "Jellyfin"
-
     def __init__(self, url, key):
         headers = {"X-MediaBrowser-Token": key}
-        super().__init__(url, headers)
+        super().__init__("Jellyfin", url, headers)
 
 
 def CreateMediaServer(type, url, key):
