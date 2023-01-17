bfe8afac378e3660a7e8ed54895cecaf858332c6
broddo <barry.broderick@icloud.com>
2023-01-16 23:43:02 -0500

    Adds new install script

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mediasync",
+    version="0.1",
+    description="Synchronizes user watch list between Emby servers and Jellyfin servers",
+    author="broddo",
+    python_requires=">=3.7",
+    packages=["mediasync"],
+    include_package_data=True,
+    install_requires=["requests>=2.28.1", "responses>=0.22.0"],
+    entry_points={
+        "console_scripts": [
+            "mediasync = mediasync.mediasyncmain:main",
+        ],
+    },
+    test_suite="unittest.defaultTestLoader.discover",
+    test_runner="unittest",
+)
